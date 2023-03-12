247b034802a72bdee25aaef2a99b3f90b42c0707
serikov1 <vasilij.serikov.03@mail.ru>
2023-03-12 13:08:20 +0300

    add new tests

--- firstLesson/alignedStorage/library.h
+++ firstLesson/alignedStorage/library.h
@@ -4,14 +4,14 @@
 #include <memory>
 #include "iostream"
 
-// &obj % Align == 0, sizeof(obj) % Size == 0
-template<std::size_t Align, std::size_t Size, typename T>
+//&obj % Align == 0, sizeof(obj) % Size == 0
+template<std::uint64_t Align, std::uint64_t Size, typename T>
 class AlignedStorage{
 public:
     template<typename... Args>
     constexpr AlignedStorage(Args ...args) {
         newPtr = reinterpret_cast<T *> ((reinterpret_cast<std::uintptr_t>(&storage) + Align - 1) & (~(Align - 1)));
-        *newPtr = T(std::forward<Args>(args)...);
+        new (newPtr) T(std::forward<Args>(args)...);
     }
 
     constexpr const T &operator*() const {
@@ -20,10 +20,76 @@ public:
     constexpr T &operator*() {
         return *newPtr;
     }
+    ~AlignedStorage() {
+        static_cast<T *>(newPtr)->~T();
+    }
 
 private:
-    std::uint8_t storage[Size + Align > sizeof (T) ? Size + Align : sizeof(T)]{};
+    std::uint8_t storage[Size + Align]{};
     T *newPtr = nullptr;
 };
+
+// This storage was proposed me by ChatGPT
+
+//template<std::size_t Align, std::size_t Size, typename T>
+//class AlignedStorage
+//{
+//private:
+//    alignas(Align) std::uint8_t data[Size];
+//
+//public:
+//    template<typename... Args>
+//   AlignedStorage(Args... args)
+//    {
+//        new (data) T(std::forward<Args>(args)...);
+//    }
+//
+//    ~AlignedStorage()
+//    {
+//        reinterpret_cast<T*>(data)->~T();
+//    }
+//
+//    T& operator*()
+//    {
+//        return *reinterpret_cast<T*>(data);
+//    }
+//
+//    const T& operator*() const
+//    {
+//        return *reinterpret_cast<const T*>(data);
+//    }
+//};
+
+// This Storage was in class
+
+//template<typename T, uint64_t Align, uint64_t Size>
+//constexpr auto storage_size() {
+//    auto estimated_size = sizeof(T) + Align;
+//    return (estimated_size + Size - 1) & (~(Size - 1));
+//}
+//
+//template<uint64_t Align, uint64_t Size, typename T>
+//class AlignedStorage {
+//public:
+//    template<typename... Args>
+//    constexpr explicit AlignedStorage(Args &&...args) {
+//        uint64_t space = sizeof(storage_);
+//        base_ = storage_;
+//        std::align(Align, sizeof(T), base_, space);
+//
+//        new (base_) T(std::forward<Args>(args)...);
+//    }
+//
+//    constexpr T &operator*() { return *static_cast<T *>(base_); }
+//    constexpr const T &operator*() const {
+//        return *static_cast<const T *>(base_);
+//    }
+//
+//    ~AlignedStorage() { static_cast<T *>(base_)->~T(); }
+//
+//private:
+//    uint8_t storage_[storage_size<T, Align, Size>()] = {};
+//    void *base_ = nullptr;
+//};
 
 #endif //ALIGNEDSTORAGE_LIBRARY_H
--- firstLesson/testStorage/main.cpp
+++ firstLesson/testStorage/main.cpp
@@ -9,7 +9,7 @@ template<uint64_t Align, uint64_t Size, typename T, typename... Args>
 constexpr auto make_storage(Args &&...args) {// HW: Read about constexpr
     return AlignedStorage<Align, Size, T>{std::forward<Args>(args)...};
 }
-TEST(AlignedStorageTest, Easy) {
+TEST(AlignedStorageTest, EasyInt) {
     static constexpr auto Align = 8UL;
     static constexpr auto Size = 16UL;
 
@@ -42,6 +42,277 @@ TEST(AlignedStorageTest, Easy) {
 
     // HW: Make similar checks for next data types: char, uint64_t, std::string, std::vector
 }
+TEST(AlignedStorageTest, EasyIntHeap) {
+    static constexpr auto Align = 8UL;
+    static constexpr auto Size = 16UL;
+
+    auto *example = new uint32_t(42);
+    auto value = make_storage<Align, Size, uint32_t>(*example);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, 42);
+    *value = 10;
+    ASSERT_EQ(*value, 10);
+
+    delete example;
+
+    // HW: Make similar checks for next data types: char, uint64_t, std::string, std::vector
+}
+TEST(AlignedStorageTest, EasyChar) {
+    static constexpr auto Align = 32UL;
+    static constexpr auto Size = 16UL;
+
+    auto value = make_storage<Align, Size, char>(13);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, 13);
+    *value = 10;
+    ASSERT_EQ(*value, 10);
+
+    // HW: Make similar checks for next data types: uint64_t, std::string, std::vector
+}
+TEST(AlignedStorageTest, EasyCharHeap) {
+    static constexpr auto Align = 32UL;
+    static constexpr auto Size = 16UL;
+    char *example = new char(13);
+    auto value = make_storage<Align, Size, char>(*example);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, 13);
+    *value = 10;
+    ASSERT_EQ(*value, 10);
+    delete example;
+    // HW: Make similar checks for next data types: uint64_t, std::string, std::vector
+}
+TEST(AlignedStorageTest, EasyUint64_t) {
+    static constexpr auto Align = 32UL;
+    static constexpr auto Size = 16UL;
+
+    auto value = make_storage<Align, Size, uint64_t>(42);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, 42);
+    *value = 10;
+    ASSERT_EQ(*value, 10);
+
+    // HW: Make similar checks for next data types std::string, std::vector
+}
+
+TEST(AlignedStorageTest, EasyString) {
+    static constexpr auto Align = 16UL;
+    static constexpr auto Size = 16UL;
+    std::string s("s");
+    auto value = make_storage<Align, Size, std::string>(s);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = (*value).size(); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+    ASSERT_EQ(*value, std::string("s"));;
+    *value = static_cast<std::string>("sss");
+
+
+    ASSERT_EQ(*value, "sss");
+
+    // HW: Make similar checks for next data types: std::vector
+}
+TEST(AlignedStorageTest, EasyStringHeap) {
+    static constexpr auto Align = 16UL;
+    static constexpr auto Size = 16UL;
+    auto *example = new std::string("s");
+    auto value = make_storage<Align, Size, std::string>(*example);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = (*value).size(); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+    ASSERT_EQ(*value, *example);
+    *value = static_cast<std::string>("sss");
+
+
+    ASSERT_EQ(*value, "sss");
+
+    delete example;
+
+    // HW: Make similar checks for next data types: std::vector
+}
+
+TEST(AlignedStorageTest, EasyVector) {
+    static constexpr auto Align = 8UL;
+    static constexpr auto Size = 128UL;
+
+    auto value = make_storage<Align, Size, std::vector<int>>((std::vector<int>{10,20}));
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = (*value).size(); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]), dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, static_cast<std::vector<int>>(std::vector<int>{10, 20}));
+    *value = std::vector<int>{10, 20, 30};
+    ASSERT_EQ(*value, static_cast<std::vector<int>>(std::vector<int>{10, 20, 30}));
+
+}
+TEST(AlignedStorageTest, EasyVectorHeap) {
+    static constexpr auto Align = 8UL;
+    static constexpr auto Size = 128UL;
+    auto example = new std::vector<int>{10, 20};
+    auto value = make_storage<Align, Size, std::vector<int>>((*example));
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = (*value).size(); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]), dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    ASSERT_EQ(*value, static_cast<std::vector<int>>(std::vector<int>{10, 20}));
+    *value = std::vector<int>{10, 20, 30};
+    ASSERT_EQ(*value, static_cast<std::vector<int>>(std::vector<int>{10, 20, 30}));
+
+    delete example;
+
+}
 
 TEST(AlignedStorageTest, Medium) {
     static constexpr auto Align = 64UL;
@@ -55,7 +326,7 @@ TEST(AlignedStorageTest, Medium) {
         uint64_t value_2;
         uint8_t value_3;
     };
-    // HW: sizeof(ProbeObject) is ???
+    fmt::print("Size of ProbeObject is: {}\n", sizeof(ProbeObject(43, 14, 28)));
     // HW: Why sizeof(ProbeObject) is not equal to sizeof(uint8_t) + sizeof(uint64_t) + sizeof(uint32_t);
 
     auto value = make_storage<Align, Size, ProbeObject>(43, 14, 28);
@@ -86,6 +357,53 @@ TEST(AlignedStorageTest, Medium) {
     ASSERT_EQ(obj.value_1, 43);
     ASSERT_EQ(obj.value_2, 14);
     ASSERT_EQ(obj.value_3, 28);
+
+}
+TEST(AlignedStorageTest, MediumHeap) {
+    static constexpr auto Align = 64UL;
+    static constexpr auto Size = 16UL;
+
+    struct ProbeObject {
+        ProbeObject(uint32_t v1, uint64_t v2, uint8_t v3)
+                : value_1{v1}, value_2{v2}, value_3{v3} {}
+
+        uint32_t value_1;
+        uint64_t value_2;
+        uint8_t value_3;
+    };
+
+    auto example = new ProbeObject(43, 14, 28);
+
+    auto value = make_storage<Align, Size, ProbeObject>(*example);
+    const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                          'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+    //Check the object alignment
+    ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+    //Check the object size
+    std::vector<uint8_t> mem_dump(Size, 0);
+    std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                mem_dump.begin());
+    for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+        ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                  dummy + sizeof(dummy));
+
+    // HW: Think how we can simplify check above.
+    // HW: Make a suggestion how we can check that copy operation is correct.
+
+    fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+    fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+               static_cast<void *>(&value), static_cast<void *>(&(*value)),
+               sizeof(value));
+
+    auto &obj = *value;
+
+    ASSERT_EQ(obj.value_1, 43);
+    ASSERT_EQ(obj.value_2, 14);
+    ASSERT_EQ(obj.value_3, 28);
+
+    delete example;
 
 }
 class ProbeObject {
@@ -155,7 +473,43 @@ TEST(AlignedStorageTest, Hard) {
     ASSERT_EQ(ProbeObject::creations_count(), 1);
     ASSERT_EQ(ProbeObject::destruction_count(), 1);
 }
-// HW: Make the same checks but with allocation on HEAP
+TEST(AlignedStorageTest, HardHeap) {
+    {
+        static constexpr auto Align = 16UL;
+        static constexpr auto Size = 32UL;
+
+        auto example = new std::vector<double>{3.14, 2.71, 9.81};
+
+        auto value =
+                make_storage<Align, Size, ProbeObject>(*example);
+        const char dummy[] = {'S', 't', 'a', 'c', ' ', 'i', 's', ' ', 'c',
+                              'o', 'r', 'r', 'u', 'p', 't', 'e', 'd'};
+
+        //Check the object alignment
+        ASSERT_EQ(reinterpret_cast<uintptr_t>(&(*value)) % Align, 0);
+        //Check the object size
+        std::vector<uint8_t> mem_dump(Size, 0);
+        std::copy_n(reinterpret_cast<const uint8_t *>(&(*value)), Size,
+                    mem_dump.begin());
+        for (auto i = sizeof(*value); i < mem_dump.size(); ++i)
+            ASSERT_EQ(std::find(dummy, dummy + sizeof(dummy), mem_dump[i]),
+                      dummy + sizeof(dummy));
+
+        // HW: Think how we can simplify check above.
+        // HW: Make a suggestion how we can check that copy operation is correct.
+
+        fmt::print("Memory dump: {:X}\n", fmt::join(mem_dump, ""));
+
+        fmt::print("Storage address: {}\nObject address: {}\nStorage size: {}\n",
+                   static_cast<void *>(&value), static_cast<void *>(&(*value)),
+                   sizeof(value));
+        delete example;
+    }
+
+    ASSERT_EQ(ProbeObject::copy_count(), 0);
+    ASSERT_EQ(ProbeObject::creations_count(), 2);
+    ASSERT_EQ(ProbeObject::destruction_count(), 2);
+}
 
 int main(int argc, char **argv)
 {
